5e461f38452735edd1e4be9ecc85c215e41449ea
Hyago Lopes Maltez <hyago.moncao@ftc.edu.br>
2020-09-21 00:15:17 -0300

    Projeto de Origem

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-ionic1
+Aplicativo facultativo.
+
+Aplicação desenvolvido e modificado com base ao ensinamento e vídeos ao decorrer da aula de programação mobile do curso de Sistemas de Informação da rede Uniftc.
+
+Api utilizada:
+http://www.omdbapi.com/
+https://console.firebase.google.com/u/0/
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD9_LNvp-VNXxEZRoKbixbbzioi4D5tSHA",
+    authDomain: "tete-6a6ec.firebaseapp.com",
+    databaseURL: "https://tete-6a6ec.firebaseio.com",
+    projectId: "tete-6a6ec",
+    storageBucket: "tete-6a6ec.appspot.com",
+    messagingSenderId: "572378286315",
+    appId: "1:572378286315:web:20f060355dfe69975416ae"
+    }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
